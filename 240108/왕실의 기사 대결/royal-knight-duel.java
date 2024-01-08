64eeee249c2211165239ff132838b5019dcafd66
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-01-08 16:41:31 +0900

    Title: 왕실의 기사 대결, Time: 131ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240108/왕실의 기사 대결/royal-knight-duel.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int l, n, q;
+    public static int[][] board = new int[41][41];
+    public static int[][] knights = new int[31][5];
+    public static int[][] newKnights = new int[31][3];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] originalHps = new int[31];
+        int answer = 0;
+
+        l = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= l; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= l; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < 5; j++) {
+                knights[i][j] = Integer.parseInt(st.nextToken());
+            }
+
+            originalHps[i] = knights[i][4];
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            moveKnight(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (knights[i][4] > 0) {
+                answer += originalHps[i] - knights[i][4];
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void moveKnight(int idx, int dir) {
+        if (knights[idx][4] <= 0) {
+            return;
+        }
+
+        if (canMove(idx, dir)) {
+            for (int i = 1; i <= n; i++) {
+                knights[i][0] = newKnights[i][0];
+                knights[i][1] = newKnights[i][1];
+                knights[i][4] -= newKnights[i][2];
+            }
+        }
+    }
+
+    public static boolean canMove(int idx, int dir) {
+        Queue<Integer> que = new LinkedList<>();
+        boolean[] visited = new boolean[31];
+        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+        for (int i = 1; i <= n; i++) {
+            newKnights[i][0] = knights[i][0];
+            newKnights[i][1] = knights[i][1];
+            newKnights[i][2] = 0;
+        }
+
+        que.offer(idx);
+        visited[idx] = true;
+
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+
+            newKnights[cur][0] += dirs[dir][0];
+            newKnights[cur][1] += dirs[dir][1];
+
+            if (newKnights[cur][0] < 1 || l < knights[cur][2] + newKnights[cur][0] - 1 || newKnights[cur][1] < 1 || l < knights[cur][3] + newKnights[cur][1] - 1) {
+                return false;
+            }
+
+            for (int i = newKnights[cur][0]; i < knights[cur][2] + newKnights[cur][0]; i++) {
+                for (int j = newKnights[cur][1]; j < knights[cur][3] + newKnights[cur][1]; j++) {
+                    if (board[i][j] == 1) {
+                        newKnights[cur][2]++;
+                    } else if (board[i][j] == 2) {
+                        return false;
+                    }
+                }
+            }
+
+            for (int i = 1; i <= n; i++) {
+                if (visited[i] || knights[i][4] <= 0) {
+                    continue;
+                }
+
+                if (knights[i][0] + knights[i][2] - 1 < newKnights[cur][0] || knights[cur][2] + newKnights[cur][0] - 1 < knights[i][0]) {
+                    continue;
+                }
+
+                if (knights[i][1] + knights[i][3] - 1 < newKnights[cur][1] || knights[cur][3] + newKnights[cur][1] - 1 < knights[i][1]) {
+                    continue;
+                }
+
+                que.offer(i);
+                visited[i] = true;
+            }
+        }
+
+        newKnights[idx][2] = 0;
+        return true;
+    }
+}
